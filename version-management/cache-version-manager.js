7a270d27bef1ce280e645d7065968a0d8b86738e
GitHub Action <action@github.com>
2026-02-10 10:27:59 +0000

    🔄 Auto-update version to 1.2.9

--- version-management/cache-version-manager.js
+++ version-management/cache-version-manager.js
@@ -8,7 +8,7 @@ class CacheVersionManager {
     constructor() {
         this.versionKey = 'app_version';
         // La version est mise à jour automatiquement par GitHub Actions
-        this.currentVersion = '1.2.8'; 
+        this.currentVersion = '1.2.9'; 
     }
 
     /**
